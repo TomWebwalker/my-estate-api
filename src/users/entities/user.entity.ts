f8ada273600ec412b2637c5d1234943d51e4763d
Tomasz Flis <tomaszwwl@gmail.com>
2022-08-12 20:38:02 +0200

    Database Fixes #3

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,18 @@
+import { ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
